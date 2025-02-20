519edc9a0dbbf3bacc6039f22046f7700b8e143e
Baptiste Grignon <51024276+ITYL-dev@users.noreply.github.com>
2025-02-20 12:03:07 +0100

    handle more than episode 0 and generalize

--- read_ds.py
+++ read_ds.py
@@ -2,13 +2,21 @@ import torch
 import os
 import cv2
 import numpy as np
-import datasets
 import json
+import pandas as pd
+from pathlib import Path
 
+dataset_name = "Ityl/so100_recording1"
+dino_ds_folder = "dataset_dino"
 
-# Función para cargar el video y convertirlo en tensor
+H, W = 224, 224
+
+user_dir = str(Path.home())
+lerobot_ds_folder = user_dir + "/.cache/huggingface/lerobot/" + dataset_name # dataset_hugging
+
 def video_to_tensor(video_path):
-    # Leer el video con OpenCV
+    """Function to load the video and convert it into a tensor"""
+    # Read the video with OpenCV
     cap = cv2.VideoCapture(video_path)
     
     frames = []
@@ -18,22 +26,23 @@ def video_to_tensor(video_path):
         if not ret:
             break
         
-        # Convertir el frame de BGR (formato OpenCV) a RGB
+        # Convert the frame from BGR (OpenCV format) to RGB
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         
-        # Redimensionar a las dimensiones deseadas (H, W)
-        frame = cv2.resize(frame, (224, 224))  # Cambia el tamaño según sea necesario
+        # Resize to desired dimensions (H, W)
+        frame = cv2.resize(frame, (H, W))
         
         frames.append(frame)
     
     cap.release()
     
-    # Convertir la lista de frames en un tensor
+    # Convert the frame list into a tensor
     video_tensor = torch.tensor(np.array(frames)).permute(0, 3, 1, 2)  # [T, H, W, C] -> [T, C, H, W]
     return video_tensor
 
-# Función para guardar los videos como archivos .pth
+
 def save_video_as_pth(video_folder, save_folder):
+    """Function to save videos as .pth files"""
     if not os.path.exists(save_folder):
         os.makedirs(save_folder)
     
@@ -42,46 +51,62 @@ def save_video_as_pth(video_folder, save_folder):
         video_path = os.path.join(video_folder, video_name)
         
         if os.path.isfile(video_path) and video_name.endswith(".mp4"):
-            print(f"Procesando {video_name}...")
+            print(f"Processing {video_name}...")
             video_tensor = video_to_tensor(video_path)
             
-            # Guardar el tensor en un archivo .pth
+            # Saving the tensor in a .pth file
             save_path = os.path.join(save_folder, f"{os.path.splitext(video_name)[0]}.pth")
             torch.save(video_tensor, save_path)
-            print(f"Guardado {save_path}")
+            print(f"Saved {save_path}")
 
-# Ruta a la carpeta con los videos .mp4
-video_folder = 'dataset_hugging/videos/chunk-000/observation.images.realsense_side'
-# Ruta donde quieres guardar los archivos .pth
-save_folder = 'dataset_dino/obses'
+# Path to the folder with .mp4 videos
+video_folder = lerobot_ds_folder + '/videos/chunk-000/observation.images.realsense_side'
+# Path where you want to save videos as .pth files
+save_folder = dino_ds_folder + '/obses'
 
-save_video_as_pth(video_folder, save_folder)
-
-import pandas as pd
-
-df = pd.read_parquet("dataset_hugging/chunk-000/episode_000000.parquet", engine="pyarrow")  # O usa "fastparquet"
-print(df.info())
-
-tensor_states = torch.tensor(np.array(df["observation.state"].to_list()))  # Convertir en tenseur PyTorch
-
-# Sauvegarder en fichier .pth dans un autre dossier
-torch.save(tensor_states, "dataset_dino/states.pth")
-
-tensor_actions = torch.tensor(np.array(df["action"].to_list()))  # Convertir en tenseur PyTorch
-
-# Sauvegarder en fichier .pth dans un autre dossier
-torch.save(tensor_actions, "dataset_dino/actions.pth")
+# save_video_as_pth(video_folder, save_folder)
 
 lengths = []
-with open("dataset_hugging/meta/episodes.jsonl", "r") as f:
+with open(lerobot_ds_folder + "/meta/episodes.jsonl", "r") as f:
     for line in f:
         data = json.loads(line)  # Convertir la ligne en dictionnaire
         lengths.append(data["length"])  # Extraire "length"
 
+with open(lerobot_ds_folder + "/meta/info.json", "r") as f:
+    data = json.load(f)  # Convertir le fichier en dictionnaire
+    action_shape = data["features"]["action"]["shape"]
+    state_shape = data["features"]["observation.state"]["shape"]
+
 # Convertir en tenseur PyTorch
 tensor_lengths = torch.tensor(lengths)
-
 # Sauvegarder dans un fichier .pth
-torch.save(tensor_lengths, "dataset_dino/seq_length.pth")
+torch.save(tensor_lengths, dino_ds_folder + "/seq_length.pth")
 
+nb_episodes = len(tensor_lengths)
+max_length = tensor_lengths.max().item()
 
+tensors_states = []
+tensors_actions = []
+
+for episode in range(nb_episodes):
+
+    df = pd.read_parquet(f"{lerobot_ds_folder}/data/chunk-000/episode_{episode:06d}.parquet", engine="pyarrow")
+    
+    tensor_states = torch.tensor(np.array(df["observation.state"].to_list()))  # Convertir en tenseur PyTorch
+    tensor_states = torch.cat((tensor_states, torch.zeros((max_length-tensor_lengths[episode], *state_shape))), dim=0) # pad zeros to max tensor length
+    tensors_states.append(tensor_states)
+
+    tensor_actions = torch.tensor(np.array(df["action"].to_list()))  # Convertir en tenseur PyTorch
+    tensor_actions = torch.cat((tensor_actions, torch.zeros((max_length-tensor_lengths[episode], *action_shape))), dim=0) # pad zeros to max tensor length
+    tensors_actions.append(tensor_actions)
+
+# Sauvegarder en fichier .pth dans un autre dossier
+tensors_states = torch.stack(tensors_states)
+torch.save(tensors_states, dino_ds_folder + "/states.pth")
+
+# Sauvegarder en fichier .pth dans un autre dossier
+tensors_actions = torch.stack(tensors_actions)
+torch.save(tensor_actions, dino_ds_folder + "/actions.pth")
+
+print(tensors_states.shape)
+print(tensors_actions.shape)
